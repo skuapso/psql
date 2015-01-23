e2cfd4d194ee3f22e6c83c3eae08df8b2e572c36
Ilya Ashchepkov <koctep@gmail.com>
2015-01-23 12:16:40 +0600

    sql:   added objects.viewable view   added sensors to objects.data view

--- sql/tables/07.uac.sql
+++ sql/tables/07.uac.sql
@@ -26,6 +26,11 @@ do also
   where user_name=new.user_name
   and array[group_id]<@"group".childs(new.group_id);
 
+create trigger insertb_00_access_denied before insert on objects._groups
+for each row
+when (not uac.can_read_group(new.parent_id))
+execute procedure triggers.reject();
+
 create view objects.groups
 with (security_barrier)
 as select
@@ -39,10 +44,58 @@ as select
   from objects._groups
   where uac.can_read_group(id);
 
-create trigger insertb_00_access_denied before insert on objects._groups
-for each row
-when (not uac.can_read_group(new.parent_id))
-execute procedure triggers.reject();
+create view objects.viewable
+as select O.*
+  from objects.groups G
+  join objects._data O on (G.id = O.group_id);
+
+create view terminals.data as
+  select T.*,'terminal'::varchar as type from objects.viewable O
+  inner join terminals._data T on (O.terminal_id=T.id)
+  where not T.deleted;
+
+create view objects.sensors as
+  select S.*,'object_sensor'::varchar as type from objects._sensors S
+  inner join objects.viewable O on (S.object_id = O.id)
+  union all
+  select *,'object_sensor'::varchar as type
+  from (
+    select
+      OT.type_id,
+      O.id,
+      OT.type_id,
+      ports,
+      provides
+    from objects.viewable O
+    inner join terminals.data T on (O.terminal_id=T.id)
+    inner join terminals.ports P on (T.model_id=P.model_id)
+    inner join objects.tools OT on (P.provides=OT.id)
+    where
+    provides is not null
+  ) S1;
+
+create view objects.data as
+  select
+    O.*,
+    array_to_json(array_agg(row_to_json(S.*)))::jsonb as sensors,
+    jsonb.extend(E.data, json_build_object('eventtime', time)) as data,
+    'object'::varchar as type
+  from objects.viewable O
+  inner join objects.sensors S on (O.id = S.object_id)
+  left join events._data E on (O.last_event_id = E.id)
+  where not O.deleted
+  group by O.id,O.no,O.model_id,
+          O.specialization_id,O.group_id,
+          O.terminal_id,O.deleted,O.last_event_id,
+          E.data,time;
+
+create view objects.get as select *,object.title(id) as title from objects.data;
+
+create view events.data as
+  select *
+  from events._data
+  where object_id in (select id from objects.viewable);
+
 create rule "create" as on insert to objects.groups
 do instead
   insert into objects._groups (title, parent_id)
@@ -57,6 +110,7 @@ do instead
   and uac.can_read_group(new.parent_id)
   and uac.can_read_group(old.parent_id)
   returning *,'group'::varchar;
+
 create rule "delete" as on delete to objects.groups
 do instead
   delete from objects._groups
@@ -64,15 +118,13 @@ do instead
   and uac.can_read_group(id)
   returning *,'group'::varchar;
 
-create view objects.data as
-  select
-    O.*,
-    'object'::varchar as type,
-    jsonb.extend(E.data, json_build_object('eventtime', time)) as data
-  from objects.groups G
-  inner join objects._data O on (G.id=O.group_id)
-  left join events._data E on (O.last_event_id = E.id)
-  where not O.deleted;
+create rule "create" as on insert to objects.data
+do instead
+  insert into objects._data (no, model_id, specialization_id, group_id, terminal_id)
+  select new.no, new.model_id, new.specialization_id, new.group_id, new.terminal_id
+  from objects.groups
+  where id=new.group_id
+  returning *, null::jsonb, null::jsonb, 'object'::varchar;
 
 create rule "update" as on update to objects.data
 do instead
@@ -88,25 +140,15 @@ do instead
     and id in (select id from objects.data where id=new.id)
     and uac.can_read_group(new.group_id)
     and uac.can_read_group(old.group_id)
-  returning *,'object'::varchar, null::jsonb;
+  returning *, null::jsonb, null::jsonb, 'object'::varchar;
+
 create rule "delete" as on delete to objects.data
 do instead
   update objects._data set deleted=true
   where id = old.id
   and id in (select id from objects.data where id=old.id)
-  returning *,'object'::varchar, null::jsonb;
-create rule "create" as on insert to objects.data
-do instead
-  insert into objects._data (no, model_id, specialization_id, group_id, terminal_id)
-  select new.no, new.model_id, new.specialization_id, new.group_id, new.terminal_id
-  from objects.groups
-  where id=new.group_id
-  returning *,'object'::varchar, null::jsonb;
+  returning *, null::jsonb, null::jsonb, 'object'::varchar;
 
-create view terminals.data as
-  select T.*,'terminal'::varchar as type from objects.data O
-  inner join terminals._data T on (O.terminal_id=T.id)
-  where not T.deleted;
 create rule r_insert as on insert to terminals.data
 do instead insert into terminals._data
   select new.id,
@@ -117,26 +159,6 @@ do instead insert into terminals._data
   returning *,'terminal'::varchar;
 alter table terminals.data alter column period set default '0:3:0';
 
-create view objects.sensors as
-  select S.*,'object_sensor'::varchar as type from objects._sensors S
-  inner join objects.data O on (S.object_id=O.id)
-  union all
-  select *,'object_sensor'::varchar as type
-  from (
-    select
-      OT.type_id,
-      O.id,
-      OT.type_id,
-      ports,
-      provides
-    from objects.data O
-    inner join terminals.data T on (O.terminal_id=T.id)
-    inner join terminals.ports P on (T.model_id=P.model_id)
-    inner join objects.tools OT on (P.provides=OT.id)
-    where
-    provides is not null
-  ) S1;
-
 create rule "create" as on insert to objects.sensors
 do instead
   insert into objects._sensors (object_id, sensor_id, port_id, provides)
@@ -144,6 +166,7 @@ do instead
   from objects.data
   where id=new.object_id
   returning *,'object_sensor'::varchar;
+
 create rule "update" as on update to objects.sensors
 do instead
   update objects._sensors
@@ -153,15 +176,9 @@ do instead
     port_id=new.port_id
   where id in (select id from objects.sensors where id=new.id and old.id=new.id)
   returning *,'object_sensor'::varchar;
+
 create rule "delete" as on delete to objects.sensors
 do instead
   delete from objects._sensors
   where id in (select id from objects.sensors where id=old.id)
   returning *,'object_sensor'::varchar;
-
-create view objects.get as select *,object.title(id) as title from objects.data;
-
-create view events.data as
-  select *
-  from events._data
-  where object_id in (select id from objects.data);
